a712a4f3e8a3aea58604283818555ae30eb36247
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-22 23:06:40 -0400

    the main logic is complete

--- CheckUnique/Program.cs
+++ CheckUnique/Program.cs
@@ -10,6 +10,30 @@ namespace CheckUnique
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a string of characters\n\n>>> ");
+            string input = Console.ReadLine();
+            int len = input.Length;
+            char[] array = input.ToCharArray();
+            bool unique = true;
+            for (int i = 0; i < len -1; i++)
+            {
+                for(int j = i + 1; j < len; j++)
+                {
+                    if(array[i] == array[j])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+            if (unique)
+            {
+                Console.WriteLine("\nAll characters in the string are unique.");
+            }
+            else
+            {
+                Console.WriteLine("\nThe string contains doubles.");
+            }
         }
     }
 }
